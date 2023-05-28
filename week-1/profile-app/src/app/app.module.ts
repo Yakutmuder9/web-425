517804f89f17961aefb6ce7982a591bbf63ed8d7
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2023-05-28 12:14:28 -0400

    profile-app

---
+++ week-1/profile-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyImageComponent } from './my-image/my-image.component';
+import { MyDetailsComponent } from './my-details/my-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyImageComponent,
+    MyDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
