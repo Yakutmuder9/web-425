dfaf8db1a454403aad26e898a5a1d73559e29c24
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2023-06-24 18:00:43 -0400

    week-5/in-n-out-booksp1

---
+++ week-5/in-n-out-booksp1/src/app/app.module.ts
@@ -0,0 +1,48 @@
+/**
+ * Title: app.module.ts
+ * Author: Professor Krasso
+ * ModifiedBy: Yakut Ahmedin
+ * Date: 24 Jun 2023
+ * Description:  In-N-Out-BookSp1 App
+*/
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BookListComponent } from './book-list/book-list.component'
+
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    BookListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ week-5/in-n-out-booksp1/src/app/book-list/book-list.component.ts
@@ -0,0 +1,22 @@
+/**
+ * Title: book-list.component.ts
+ * Author: Professor Krasso
+ * ModifiedBy: Yakut Ahmedin
+ * Date: 24 Jun 2023
+ * Description:  In-N-Out-BookSp1 App
+*/
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss']
+})
+export class BookListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
