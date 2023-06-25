ea238fffe54ee570d985d1b70e79163863a45a32
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2023-06-24 20:48:33 -0400

    week-5/in-n-out-booksp3

---
+++ week-5/in-n-out-booksp3/src/app/app.component.ts
@@ -0,0 +1,21 @@
+/**
+ * Title: app.component.ts
+ * Author: Professor Krasso
+ * ModifiedBy: Yakut Ahmedin
+ * Date: 24 Jun 2023
+ * Description:  In-N-Out-BookSp1 App
+*/
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  assignment: string;
+
+  constructor() {
+    this.assignment = 'Assignment 5.4 - Dialogs'
+  }
+}
